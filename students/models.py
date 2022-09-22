d817bc08777a0ef62ca52c31fff8cff1d8ec9f10
Bazan Nikolay <bazan.nick.art@gmail.com>
2022-09-22 14:18:04 +0300

    Lesson 13

--- students/models.py
+++ students/models.py
@@ -1,7 +1,14 @@
 from datetime import date
 
+from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
+
+from faker import Faker
+
+from .validators import ValidEmailDomain
+
+VALID_DOMAIN_LIST = ('@gmail.com', '@yahoo.com', '@test.com')
 
 
 class Student(models.Model):
@@ -19,9 +26,26 @@ class Student(models.Model):
         error_messages={'min_length': '"last_name" field value less than two symbols'}
     )
     birthday = models.DateField(default=date.today, null=True, blank=True)
+    email = models.EmailField(validators=[ValidEmailDomain(*VALID_DOMAIN_LIST)])
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        db_table = 'student_table'
+        db_table = 'students'
+
+    @classmethod
+    def generate_fake_data(cls, cnt):
+        f = Faker()
+
+        for _ in range(cnt):
+            first_name = f.first_name()
+            last_name = f.last_name()
+            email = f'{first_name}.{last_name}{f.random.choice(VALID_DOMAIN_LIST)}'
+            birthday = f.date()
+            st = cls(first_name=first_name, last_name=last_name, birthday=birthday, email=email)
+            try:
+                st.full_clean()
+                st.save()
+            except ValidationError:
+                print('Incorrect data.')
