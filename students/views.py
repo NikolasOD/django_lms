66b8949ecc096e040941f3eccc4c7f2ca79f7642
Bazan Nikolay <bazan.nick.art@gmail.com>
2022-09-26 20:08:37 +0300

    Lesson 14

--- students/views.py
+++ students/views.py
@@ -1,14 +1,68 @@
-from django.http import HttpResponse
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+from django.middleware.csrf import get_token
 from django.shortcuts import render # noqa
 
+from webargs.djangoparser import use_args
+from webargs.fields import Str
+
+from .forms import CreateStudentForm
 from .models import Student
+from .utils import qs2html
 
 
 def index(request):
+    return HttpResponse('Welcome to LMS!')
+
+
+@use_args(
+    {
+        'first_name': Str(required=False),
+        'last_name': Str(required=False),
+    },
+    location='query'
+)
+def get_students(request, args):
     students = Student.objects.all()
-    s = '<table>'
-    for student in students:
-        s += f'<tr><td>{student.first_name}</td><td>{student.last_name}</td><td>{student.email}</td></tr>'
-    s += '</table>'
-    response = HttpResponse(s)
+
+    if len(args) != 0 and args.get('first_name') or args.get('last_name'):
+        students = students.filter(
+            Q(first_name=args.get('first_name', '')) | Q(last_name=args.get('last_name', ''))
+        )
+
+    html_form = '''
+        <form method="get">
+          <label for="first_name">First name:</label>
+          <input type="text" id="first_name" name="first_name" placeholder="John"><br><br>
+          <label for="last_name">Last name:</label>
+          <input type="text" id="last_name" name="last_name" placeholder="Doe"><br><br>
+          <input type="submit" value="Submit">
+        </form>
+    '''
+
+    html = qs2html(students)
+    response = HttpResponse(html_form + html)
     return response
+
+
+def create_student(request):
+    if request.method == 'GET':
+        form = CreateStudentForm()
+    elif request.method == 'POST':
+        form = CreateStudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/students/')
+
+    token = get_token(request)
+    html_form = f'''
+        <form method="post">
+          <input type="hidden" name="csrfmiddlewaretoken" value="{token}">
+          <table>
+            {form.as_table()}
+          </table>
+          <input type="submit" value="Submit">
+        </form>
+    '''
+
+    return HttpResponse(html_form)
