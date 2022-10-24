b014c223d4292f50a909ebf836323d792fa5bfa7
Bazan Nikolay <bazan.nick.art@gmail.com>
2022-10-24 21:46:48 +0300

    Home work 23. Admin panel.

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin    # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import Group
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'group_start_date', 'headman_name')
+    list_display_links = list_display
+    list_per_page = 15
+    fieldsets = (
+        (None, {'fields': ('group_name',)}),
+        ('Dates', {'fields': ('group_start_date', 'group_end_date')}),
+        ('Relations', {'fields': ('headman', 'teachers')}),
+        ('Group description', {'fields': ('group_description',), 'classes': ('collapse',)}),
+    )
+
+    def headman_name(self, instance):
+        if instance.headman:
+            return f'{instance.headman.first_name} {instance.headman.last_name}'
+        return ''
+
+    headman_name.short_description = 'headman'
+
+
+admin.site.register(Group, GroupAdmin)
